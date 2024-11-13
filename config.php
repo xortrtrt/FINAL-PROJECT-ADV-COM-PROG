95134905a9b0edfeb9e3e7db3d3f1741632b6fb5
xortrtrt <23-32737@g.batstate-u.edu.ph>
2024-11-13 21:28:54 +0800

    updated config
    inalis ko na yung displays connection successful kasi hindi nawawala and hindi rin naman kailangan

--- config.php
+++ config.php
@@ -8,10 +8,4 @@ $dbname = "skill_development_portal";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "Connected successfully to the database!";
-}
 ?>
